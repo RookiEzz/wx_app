052a5dceef666296a3fcdf7b05516268844d3382
renhai <renxuanxun@gmail.com>
2017-03-26 01:47:37 +0800

    3.26

--- pages/music/music.js
+++ pages/music/music.js
@@ -4,10 +4,11 @@ Page({
     classname:'',
     current:'',
     isplay: false,
-    music_url:""
+    music_url:"",
+    isFirst:true
   },
   list:[],
-  onLoad:function(options){
+  onLoad:function(option){
     var that = this;
     wx.request({
       url: 'http://v3.wufazhuce.com:8000/api/channel/music/more/0?channel=wdj&version=4.0.2&uuid=ffffffff-a90e-706a-63f7-ccf973aae5ee&platform=android',
@@ -17,14 +18,19 @@ Page({
         console.log(tempList)
         var len = tempList.length;
         that.setData({
-          list : res.data.data
+          list : res.data.data,
+          
         })
-      },
-      fail: function() {
-        // fail
-      },
-      complete: function() {
-        // complete
+        var url_list = [];
+        wx.request({
+          url: 'https://app.mawenbao.com/music-api-server/?p=xiami&t=songlist&i='+res.data.data.audio_url,
+          
+          success: function(res){
+            // success
+          }
+        })
+       
+        
       }
     })
   },
@@ -55,6 +61,7 @@ Page({
     var music_id = option.currentTarget.dataset.musicid 
     console.log("id?")
     console.log(music_id)
+    var that = this;
   wx.request({
     url: 'https://app.mawenbao.com/music-api-server/?p=xiami&t=songlist&i='+music_id,
     success: function(res){
@@ -64,6 +71,7 @@ Page({
       that.setData({
         music_url:music_url
       })
+      
     },
     fail: function() {
       // fail
@@ -75,22 +83,24 @@ Page({
   var current = option.currentTarget.dataset.index
   console.log('点击事件')
   console.log(this)
-  if(!this.data.isplay){
+  if(this.data.isFirst){
+    console.log('创建音乐')
+     this.audioCtx = wx.createAudioContext('myAudio')
+     this.audioCtx.setSrc(this.data.music_url)
+      this.audioCtx.play()
+     console.log(this.data)
+     this.setData({
+       isFirst: false
+     })
+  }
+  if(!this.data.isFirst){
+    this.audioCtx = wx.createAudioContext('myAudio')
+    this.audioCtx.setSrc(this.data.music_url)
+    if(!this.data.isplay){
     console.log('没有播放')
-    var that = this;
     console.log(this.data)
-    wx.playVoice({
-      filePath: "http://om5.alicdn.com/905/2103135905/2102717758/1795677918_1490068765995.mp3?auth_key=2f513e4c043db309a0c5c0dde62ced5a-1491015600-0-null",
-      success: function(res){
-        // success
-      },
-      fail: function(err) {
-        console.log(err)
-      },
-      complete: function() {
-        // complete
-      }
-    })
+    this.audioCtx.play()
+    console.log("开始播放")
     this.setData({
       classname:'mystyle ',
       current:current,
@@ -100,6 +110,7 @@ Page({
     console.log(isplay)
     }else{
       console.log('在播放')
+      this.audioCtx.pause()
       this.setData({
       classname:'',
       current:current,
@@ -108,10 +119,12 @@ Page({
     
    
     }
+  }
+  
 
   },
   onReady:function(){
-    this.audioCtx = wx.createAudioContext('myAudio');
+    
   },
   onShow:function(){
     // 页面显示
